ca84a9d81109151e5ffd5ab235c3c60ccc0979a3
Ambuj Ojha <ambujoj@gmail.com>
2020-12-03 21:27:18 -0500

    Added imdb in domain analysis

--- create_sub_scripts.py
+++ create_sub_scripts.py
@@ -6,7 +6,7 @@ command_list = ["#!/bin/bash",
 "#SBATCH --ntasks-per-node=1",
 "#SBATCH --cpus-per-task=2",
 "#SBATCH --time=24:00:00",
-"#SBATCH --mem=4GB",
+"#SBATCH --mem=32GB",
 "#SBATCH --gres=gpu:1",
 "#SBATCH --job-name=nlp_project",
 "#SBATCH --mail-type=END",
@@ -19,7 +19,7 @@ command_list = ["#!/bin/bash",
 "source ~/pyenv/py3.6.3/bin/activate"]
 
 task_names = ["sst2", "imdb"]
-roberta_versions = ["roberta", "roberta-large"]
+roberta_versions = ["roberta-base", "roberta-large"]
 num_epochs = [3, 10]
 max_seq_lengths = [128]
 batch_sizes = [16, 32]
@@ -29,16 +29,17 @@ for task in task_names:
     for epoch in num_epochs:
         for batch_size in batch_sizes:
             for learning_rate in learning_rates:
-                prefix = str(task) + "_" + str(epoch) + "_" + str(batch_size) + "_" + learning_rate 
-                job_file = "submission_scripts/" + prefix + ".sh"
-                cache_dir = prefix + "_cache"
-                output_dir = prefix + "_output"
+                for roberta_version in roberta_versions:
+                    prefix = roberta_version + "_" + str(task) + "_" + str(epoch) + "_" + str(batch_size) + "_" + learning_rate 
+                    job_file = "submission_scripts/" + prefix + ".sh"
+                    cache_dir = "caches/" + prefix + "_cache"
+                    output_dir = "outputs/" + prefix + "_output"
               
-                command = "python roberta_fine_tune.py --task_name " +  task + " --cache_dir " + cache_dir + " --num_epochs " + str(epoch) + " --batch_size " + str(batch_size) + " --learning_rate " + learning_rate + " --output_dir " + output_dir
+                    command = "python roberta_fine_tune.py --task_name " +  task + " --roberta_version " + roberta_version + " --cache_dir " + cache_dir + " --num_epochs " + str(epoch) + " --batch_size " + str(batch_size) + " --learning_rate " + learning_rate + " --output_dir " + output_dir
 
-                with open(job_file, "w") as f:
-                    for cmd in command_list:
-                        f.write(cmd + "\n")
-                    f.write(command + "\n")
+                    with open(job_file, "w") as f:
+                        for cmd in command_list:
+                            f.write(cmd + "\n")
+                        f.write(command + "\n")
 
 
---
+++ roberta/imdb_hp_eval.py
@@ -0,0 +1,112 @@
+# import dependencies
+import argparse
+import os
+
+import numpy as np
+import sklearn
+from sklearn.utils import shuffle
+import torch
+import torch.nn as nn
+from torch.utils.data import (DataLoader, RandomSampler, TensorDataset)
+from tqdm import tqdm
+
+import transformers
+from transformers import (AutoTokenizer, AutoModelForSequenceClassification)
+from transformers import set_seed
+
+import datasets
+from datasets import load_dataset
+
+
+def main():
+    # create argument parser
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--max_seq_length', type=int, default=128, help='Maximum sequence length of the inputs')
+    parser.add_argument('--seed', type=int, default=42, help='Random seed')
+
+    args = parser.parse_args()
+
+    # set device
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+    # set seed
+    set_seed(args.seed)
+
+    # load dataset
+    dataset = load_dataset('glue', 'sst2', split='validation')
+    sentences, labels = shuffle(dataset['sentence'], dataset['label'], random_state=args.seed)
+    split_idx = int(0.2*len(sentences))
+    dev_data = {'sentence': sentences[:split_idx], 'label': labels[:split_idx]}
+
+    # create file to log accuracies
+    acc_file = open('accuracy.txt', 'w+')
+
+    for model_type in ['rob_base_imdb', 'imdb']:
+        for epochs in ['3', '10']:
+            for batch_size in ['16', '32']:
+                for learning_rate in ['1e-5', '3e-6', '3e-5', '5e-5']:
+                    #imdb_3_16_5e-5_output/
+                    #rob_base_imdb_10_16_1e-5_output/
+                    directory = 'outputs/' + model_type + '_' + epochs + '_' + batch_size + '_' + learning_rate + '_' + 'output'
+
+                    if directory == "outputs/rob_base_imdb_10_16_3e-6_output":
+                        continue
+                    # load tokenizer and model
+                    print('Loading tokenizer and model for', model_type, epochs, batch_size, learning_rate)
+
+                    tokenizer = AutoTokenizer.from_pretrained(directory)
+                    model = AutoModelForSequenceClassification.from_pretrained(directory).to(device)
+
+                    # process dataset
+                    print('Processing dataset')
+
+                    padding = 'max_length'
+                    features = tokenizer(dev_data['sentence'], padding=padding, max_length=args.max_seq_length, truncation=True)
+                    labels = dev_data['label']
+
+                    all_input_ids = torch.tensor([f for f in features.input_ids], dtype=torch.long)
+                    all_attention_mask = torch.tensor([f for f in features.attention_mask], dtype=torch.long)
+                    all_labels = torch.tensor([f for f in labels], dtype=torch.long)
+
+                    tensor_dataset = TensorDataset(all_input_ids, all_attention_mask, all_labels) 
+                    sampler = RandomSampler(tensor_dataset)
+                    dataloader = DataLoader(tensor_dataset, sampler=sampler, batch_size=int(batch_size))
+
+                    # evaluate model
+                    print('Evaluating model')
+
+                    preds = None
+                    gold_labels = None
+
+                    step = None
+                    eval_iterator = tqdm(dataloader, desc='Evaluating')
+                    for step, batch in enumerate(eval_iterator):
+                        model.eval()
+                        batch = tuple(t.to(device) for t in batch)
+
+                        with torch.no_grad():
+                            inputs = {'input_ids': batch [0].to(device), 'attention_mask': batch[1].to(device), 'labels': batch[2].to(device)}
+                            labels = batch[2].to(device)
+
+                            out = model(**inputs)[1].double().to(device)
+
+                            if preds is None:
+                                preds = out.detach().cpu().numpy()
+                                gold_labels = labels.detach().cpu().numpy()
+                            else:
+                                preds = np.append(preds, out.detach().cpu().numpy(), axis=0)
+                                gold_labels = np.append(gold_labels, labels.detach().cpu().numpy(), axis=0)
+
+                    # compute accuracy
+                    preds = np.argmax(preds, axis=1)
+                    accuracy = np.sum(preds == gold_labels)/len(preds)
+                    print('eval accuracy: {}'.format(accuracy))
+
+                    # write accuracy to file
+                    acc_file.write(model_type + '_' + epochs + '_' + batch_size + '_' + learning_rate + ' ' + str(accuracy) + '\n')
+    
+    acc_file.close()
+
+if __name__ == '__main__':
+    main()
